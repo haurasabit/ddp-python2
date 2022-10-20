9e343cefb67b1cc0169eb0aa1e94983f05868e79
haurasabit <sabithaura@gmail.com>
2022-10-20 10:34:25 +0630

    belajar variable

---
+++ index.py
@@ -0,0 +1,43 @@
+a = 1 
+print (a)
+
+nama = "Haura"
+print (nama)
+
+NamaLengkap = "Haura Tsabitah"
+print(NamaLengkap)
+
+tempat_tanggal_lahir = "Jakarta, 11 Agustus 2004"
+print (tempat_tanggal_lahir)
+
+NamaKampus = "STT Terpadu Nurul Fikri"
+rombel_saya = "TI03"
+nim_saya ="0110222242"
+print(nim_saya,rombel_saya,NamaKampus)
+
+HobiSaya = "nonton", "shopping", "jalan-jalan"
+print(HobiSaya)
+
+MakananKesukaan = "apapun yang penting halal, mecin, GRATIS ygy!"
+print(MakananKesukaan)
+
+umur_saya= str(18)
+print(umur_saya)
+
+tahun_lulus_smp = int(2019)
+tahun_lulus_sma = int(2022)
+print(tahun_lulus_smp,tahun_lulus_sma)
+
+goals_tinggi_dalam_meter = float(1.65)
+print(goals_tinggi_dalam_meter)
+
+umur_saya_sekarang ="saya 18 tahun"
+umur_adik_saya = "adik saya 12"
+hasil = "pasti tua an saya lhaaa"
+PythonSay = "Python Say.."
+
+print(umur_saya_sekarang,umur_adik_saya)
+print(hasil)
+print(PythonSay)
+print(18>12)
+
